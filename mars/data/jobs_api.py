6115821ffdcc03bab6507357b212a2e57c8f5fe7
mrKONDR515 <132822740+mrKONDR515@users.noreply.github.com>
2025-03-19 18:15:03 +0400

    Add files via upload

--- mars/data/jobs_api.py
+++ mars/data/jobs_api.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import flask
 from flask import jsonify, make_response, request
 
@@ -46,16 +48,57 @@ def create_jobs():
     elif not all(key in request.json for key in
                  ['job', 'work_size', 'collaborators', 'team_leader', 'start_date', 'end_date', 'is_finished']):
         return make_response(jsonify({'error': 'Bad request'}), 400)
+    try:
+        start_date = datetime.strptime(request.json['start_date'], '%Y-%m-%d %H:%M:%S')
+        end_date = datetime.strptime(request.json['end_date'], '%Y-%m-%d %H:%M:%S')
+    except ValueError:
+        return make_response(jsonify({'error': 'Bad request'}), 400)
     db_sess = db_session.create_session()
     jobs = Jobs(
         job=request.json['job'],
         work_size=request.json['work_size'],
         collaborators=request.json['collaborators'],
-        start_date=request.json['start_date'],
-        end_date=request.json['end_date'],
+        start_date=start_date,
+        end_date=end_date,
         is_finished=request.json['is_finished'],
         team_leader=request.json['team_leader'],
     )
     db_sess.add(jobs)
     db_sess.commit()
     return jsonify({'id': jobs.id})
+
+
+@blueprint.route('/api/jobs/<int:jobs_id>', methods=['DELETE'])
+def delete_jobs(jobs_id):
+    db_sess = db_session.create_session()
+    jobs = db_sess.query(Jobs).get(jobs_id)
+    if not jobs:
+        return make_response(jsonify({'error': 'Not found'}), 404)
+    db_sess.delete(jobs)
+    db_sess.commit()
+    return jsonify({'success': 'OK'})
+
+
+@blueprint.route('/api/jobs/<int:jobs_id>', methods=['PUT'])
+def edit_jobs(jobs_id):
+    db_sess = db_session.create_session()
+    jobs = db_sess.query(Jobs).get(jobs_id)
+
+    if not request.json:
+        return make_response(jsonify({'error': 'Empty request'}), 400)
+    elif not all(key in request.json for key in
+                 ['job', 'work_size', 'collaborators', 'team_leader', 'start_date', 'end_date', 'is_finished']):
+        return make_response(jsonify({'error': 'Bad request'}), 400)
+
+    for key in ['job', 'work_size', 'collaborators', 'team_leader', 'start_date', 'end_date', 'is_finished']:
+        if key in request.json:
+            if key in ['start_date', 'end_date']:
+                try:
+                    setattr(jobs, key, datetime.strptime(request.json[key], '%Y-%m-%d %H:%M:%S'))
+                except ValueError:
+                    return make_response(jsonify({'error': 'Bad request'}), 400)
+            else:
+                setattr(jobs, key, request.json[key])
+
+    db_sess.commit()
+    return jsonify({'success': 'OK'})
--- mars/data/test.py
+++ mars/data/test.py
@@ -1,27 +1,49 @@
-from requests import get, post, delete
+from requests import get, post, delete, put
 
 # print(get('http://localhost:5000/api/jobs').json())
 # print(get('http://localhost:5000/api/jobs/1').json())
 # print(get('http://localhost:5000/api/jobs/999').json())
 # # новости с id = 999 нет в базе
 # print(get('http://localhost:5000/api/jobs/q').json())
-print(post('http://localhost:5000/api/jobs', json={}).json())
-# Пустой запрос
-print(post('http://localhost:5000/api/jobs',
-           json={'id': '1'}).json())
-print(post('http://localhost:5000/api/jobs',
-           json={'work_size': '15'}).json())
+# print(post('http://localhost:5000/api/jobs', json={}).json())
+# # Пустой запрос
+# print(post('http://localhost:5000/api/jobs',
+#            json={'id': '1'}).json())
+# print(post('http://localhost:5000/api/jobs',
+#            json={'work_size': '15'}).json())
 # Введены не все данные
-print(post('http://localhost:5000/api/jobs',
-           json={'job': 'cabin cleaning',
-                 'work_size': '10',
-                 'collaborators': '4',
-                 'start_date': '2025-03-16 20:39:32',
-                 'end_date': '2025-03-16 20:39:32',
-                 'is_finished': True,
-                 'team_leader': 2}).json())
-# Верный запрос, но почему-то он не проходит
+# print(post('http://localhost:5000/api/jobs',
+#            json={'job': 'cabin cleaning',
+#                  'work_size': '10',
+#                  'collaborators': '4',
+#                  'start_date': '2025-03-16 20:39:32',
+#                  'end_date': '2025-03-16 20:39:32',
+#                  'is_finished': True,
+#                  'team_leader': 2}).json())
+# Верный запрос
 # print(delete('http://localhost:5000/api/jobs/999').json())
-# # новости с id = 999 нет в базе
+# print(delete('http://localhost:5000/api/jobs/998').json())
+# print(delete('http://localhost:5000/api/jobs/997').json())
+# print(delete('http://localhost:5000/api/jobs/996').json())
+# # работ с такими id нет в базе
 # print(delete('http://localhost:5000/api/jobs/1').json())
+# # Верный запрос
+# print(put('http://localhost:5000/api/jobs/999',
+#           json={"job": "Very hard job",
+#                 "work_size": 15}).json())
+# # работы с такими id нет в базе
+# print(put('http://localhost:5000/api/jobs/1', json={}).json())
+# # Пустой запрос
+# print(put('http://localhost:5000/api/jobs/1',
+#           json={"start_date": "20-03-2025 12:00:00"}).json())
+# # Неверный формат даты
+# print(put('http://localhost:5000/api/jobs/1',
+#           json={"job": "Updated job",
+#                 "work_size": 20,
+#                 "collaborators": "2, 3",
+#                 "team_leader": 2,
+#                 "start_date": "2025-03-20 12:00:00",
+#                 "end_date": "2025-03-20 18:00:00",
+#                 "is_finished": False}).json())
+# # Верный запрос
 print(get('http://localhost:5000/api/jobs').json())
