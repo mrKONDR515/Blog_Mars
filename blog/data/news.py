fc9114fe023830769325a10aef192c1745f9f746
mrKONDR515 <132822740+mrKONDR515@users.noreply.github.com>
2025-03-20 19:38:04 +0400

    Add files via upload

--- blog/data/news.py
+++ blog/data/news.py
@@ -20,3 +20,6 @@ class News(SqlAlchemyBase, SerializerMixin):
     user_id = sqlalchemy.Column(sqlalchemy.Integer,
                                 sqlalchemy.ForeignKey("users.id"))
     user = orm.relationship('User')
+    categories = orm.relationship("Category",
+                                  secondary="association",
+                                  backref="news")
---
+++ blog/data/test2.py
@@ -0,0 +1,17 @@
+from requests import get, post, delete
+
+print(get('http://localhost:5000/api/v2/news').json())
+# print(get('http://localhost:5000/api/v2/news/1').json())
+# print(get('http://localhost:5000/api/v2/news/999').json())
+# новости с id = 999 нет в базе
+# print(post('http://localhost:5000/api/v2/news', json={}).json())
+# print(post('http://localhost:5000/api/v2/news',
+#            json={'title': 'Заголовок'}).json())
+# print(post('http://localhost:5000/api/v2/news',
+#            json={'title': 'Заголовок',
+#                  'content': 'Текст новости',
+#                  'user_id': 1,
+#                  'is_private': False}).json())
+# print(delete('http://localhost:5000/api/v2/news/999').json())
+# новости с id = 999 нет в базе
+# print(delete('http://localhost:5000/api/v2/news/1').json())
